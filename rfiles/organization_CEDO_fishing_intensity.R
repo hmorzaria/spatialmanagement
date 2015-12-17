47134d10148a4a8de0f4c91e5f4c4f7eb57e719e
Hem Nalini Morzaria <hmorzaria@hotmail.com>
2015-12-17 10:56:29 -0800

    Revised and annotated. For Pangas data changed normalization to between minimum and 1

--- rfiles/organization_CEDO_fishing_intensity.R
+++ rfiles/organization_CEDO_fishing_intensity.R
@@ -2,74 +2,65 @@
 #' organize shapefiles from CEDO surveys
 #' for fishery intensity project
 #' hmorzarialuna@gmail.com
+#' Last revised December 2015
 #' install.packages(c("ggplot2","reshape","RColorBrewer","classInt","maptools","rgeos"))
+#' Based on code by Zia Ahmed zua3 at cornell.edu 
 
 x = c("gdata","ggplot2","reshape","dplyr","RColorBrewer","classInt",
-      "rgdal","rgeos","maptools","raster","data.table","wesanderson", "plyr")
+      "rgdal","rgeos","magrittr","maptools","raster","data.table","wesanderson", "plyr")
 
 lapply(x, require, character.only = TRUE)
 
+#' remove all files
 rm(list=ls())
-
+#' graphics off
 graphics.off()
 
-pathToSaveShapes = "E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Zonation/Scenarios_Nov2015/Original_data/"
-mainpath = "E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Zonation/"
+pathToSaveShapes = "E:/Archivos/1Archivos/Articulos/En preparacion/Spatial_management/Analisis/Zonation/Scenarios_Nov2015/Original_data/"
+mainpath = "E:/Archivos/1Archivos/Articulos/En preparacion/Spatial_management/Analisis/Zonation/"
+framepath ="E:/Archivos/1Archivos/Articulos/En preparacion/Spatial_management/Analisis/Zonation/Scenarios_Nov2015/Frame"
 
 setwd("E:/Archivos/1Archivos/Articulos/En preparacion/Fisheries_intensity")
 
 cedo_data = fread("cedo_fishing_records.csv")
 
-#List of locations and acronyms
+#' List of locations and acronyms
 names_locations = unique(cedo_data$CODE)
 
-#Species names and prices
+#' Species names and prices
 species_data = fread("species_names.csv", header=TRUE)
 
-#Species names
+#' Species names
 species_names = unique(cedo_data$SP_CODE)
 
-## make rasters per species per community
-##Based on code by Zia Ahmed zua3 at cornell.edu 
-###############################################################################
-
-# Get list of all shape files from directory
-#------------------------------------------------------------
-
-setwd("E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Zonation/Scenarios_Nov2015/Frame")
+#' Get corridor polygon and raster frame
+setwd(framepath)
 poly.r = readOGR(".", "corridor_wetland_polygon")
 north.r = raster("Frame_corridor_final.tif")
-#define projection of data
+#' Define projection of data
 proj.lcc = proj4string(north.r)
 crs.geo <- CRS(proj.lcc)
 
+#' Create blank raster
 e.shape <- extent(north.r)
-#ras.rows = nrow(north.r)
-#ras.cols = ncol(north.r)
-
 mask.raster = raster(e.shape,resolution=c(1000,1000),crs=crs.geo)
-
-#set background values to 0
+#' set background values to 0
 mask.raster[is.na(mask.raster)] <- 0
 
 setwd(pathToSaveShapes)
 
+#' make rasters per species per community
+
 for (eachfolder in 1:length(names_locations)) 
-  
 {
   this.folder = names_locations[eachfolder]
-  
   print(this.folder)
-
   folder.data = cedo_data %>% 
     tbl_df %>% 
     filter(CODE==this.folder)
-
   unique.sp = unique(folder.data$SP_CODE)
-   
 
   for(eachsp in 1:length(unique.sp))
-
       {
     sp.r = mask.raster
     this.sp = unique.sp[eachsp]
@@ -81,77 +72,74 @@ print(this.sp)
     
     sp.data$W = 1
     
-    biodiv.rows = nrow(sp.data)
-    
-    iterations = round(biodiv.rows/1000,0)
-    
+ #' if data for species has > 1000 row divide in chunks
+ #' of 1000 for quicker analysis  
+  
     if(nrow(sp.data)>1000)
     {
       last.row = 1001
       first.row = 1
+      iterations = sp.data %>% nrow %>% divide_by(1000)%>% round(0)
       
       for (each.iteration in 1:iterations)
-        
       {
         new.last.row = ((last.row-1)*each.iteration)
-        
-        section.biodiv = sp.data[first.row:new.last.row,] %>% 
-         mutate_each(funs(as.numeric),LAT,LON) %>% 
-          as.data.frame
-        
-        section.biodiv = section.biodiv[!is.na(section.biodiv$LAT),]
-        
+   #' divide data in section of 1000 rows (or less)
+    section.biodiv = sp.data[first.row:new.last.row,] %>% 
+    mutate_each(funs(as.numeric),LAT,LON) %>% 
+    as.data.frame
+   #' eliminate NAs  
+   section.biodiv = section.biodiv[!is.na(section.biodiv$LAT),]
+   #' make spatial     
    coordinates(section.biodiv) = c("LON","LAT")
-    
+   #' define and assign projection
    crs.wgs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
    proj4string(section.biodiv) <- crs.wgs
    section.biodiv = spTransform(section.biodiv, crs.geo)
-   stations_subset <- section.biodiv[poly.r, ]
-   
-      }}
-    
+   #' eliminate points outside of corridor 
+    stations_subset <- section.biodiv[poly.r, ]
+         }}
+    #' for data sets of < 1000 rows
     if(nrow(sp.data)<1000){
-      
+      #' convert into data frame
       section.biodiv = sp.data %>% 
        mutate_each(funs(as.numeric),LAT,LON) %>% 
         as.data.frame
-      
+      #' eliminate NAs
       section.biodiv = section.biodiv[!is.na(section.biodiv$LAT),]
-      
+      #' make spatial
       coordinates(section.biodiv) = c("LON","LAT")
-      
-      crs.wgs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  # geographical, datum WGS84
+      #' assign geographical, datum WGS84 and transform to lcc
+      crs.wgs <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84")  
       proj4string(section.biodiv) <- crs.wgs
       section.biodiv = spTransform(section.biodiv, crs.geo)
+      #' eliminate points outside of corridor
       stations_subset <- section.biodiv[poly.r, ]
           }
-      
     data_frame_species <- as(stations_subset, "data.frame")
     
+    sp.r = mask.raster
+    #' make each point a raster
     for(eachline in 1:nrow(data_frame_species))
-      
-    {
+      {
       sp.data.geo = data_frame_species[eachline,]
       coordinates(sp.data.geo) = c("LON","LAT")
       proj4string(sp.data.geo) <- crs.geo
-      #rasterize using mask raster
+      #' rasterize using mask raster
     out.r <- rasterize(sp.data.geo, mask.raster,field=sp.data.geo@data$W) 
-   
-    # set the cells associated with the shapefile to the specified value
+      #' set the cells associated with the shapefile to the specified value
     out.r[is.na(out.r)] <- 0
-    
-    sp.r = sum(sp.r, out.r)
-    
-  }#end lines
+     sp.r = sum(sp.r, out.r)
+    }#' end lines
   
-   #raster per community species
+   #' raster per community species
   writeRaster(sp.r, filename=paste(this.folder,this.sp,"cedo_sp",sep="_"), format="GTiff", overwrite=TRUE)  
   
-}# end species
+}#' end species
   print(paste("Done with species",this.sp,sep=""))
-} # end folder
-#obtain raster sum all species per community
+} #' end folder
 
+#' obtain raster sum all species per community
 print("Calculating rasters per community")
 
 setwd(pathToSaveShapes)
@@ -159,129 +147,101 @@ setwd(pathToSaveShapes)
 for(eachlocation in 1:length(names_locations))
 {
   this.location = names_locations[eachlocation] 
-  
   print(paste("Analyzing",this.location,sep=" "))
-  
+  #' list all rasters
   shape.sp=list.files(getwd(),  pattern=glob2rx(paste(this.location,"*sp.tif",sep="")), full.names=FALSE)
-  
-  all.sp.loc = stack(shape.sp)
-
-  all.r = sum(all.sp.loc)    
-  
-  species.indx = all.sp.loc / all.r #will this work
-  
-  r.layers = names(all.sp.loc)
-  
-   for (eachlayer in 1:length(r.layers)){
-    
-     name.sp.folder = unlist(strsplit(r.layers[eachlayer],"_"))[2]
-    
-     r <- raster(species.indx, layer=eachlayer)
-     
+  #' stack rasters and divide by sum of all rasters in stack, output is brick layer
+  species.indx = shape.sp %>%  stack %>% divide_by(shape.sp %>%  stack %>% sum)
+  #' get layer names in stack
+  #' brick layer loses names
+  r.layers = shape.sp %>%  stack %>% names
+  #' save each layer in the raster brick separately
+  for (eachlayer in 1:length(r.layers)){
+    name.sp.folder = unlist(strsplit(r.layers[eachlayer],"_"))[2]
+    r <- raster(species.indx, layer=eachlayer)
     writeRaster(r, filename=paste(this.location,name.sp.folder,"cedo_In",sep="_"), format="GTiff", overwrite=TRUE)  
-
-    species.price = species_data %>% 
-      filter(NewCode==name.sp.folder) %>% 
-      .$Price %>% 
-      as.numeric %>% .[1]
-    r.economic = r * species.price
-    
-    writeRaster(r.economic, filename=paste(this.location,name.sp.folder,"cedo_IE",sep="_"), format="GTiff", overwrite=TRUE)  
-    
 } # close locations
 }
 
+#' Read in all species across communities and average
 
-#Read in all species across communities and average
-#
 print("Averaging species across communities")
 
 for(eachspecies in 1:length(species_names))
 {
   this.species = species_names[eachspecies] 
-
   print(paste("Analyzing",this.species,sep=" "))
-  
+  #' lists all rasters for that species
   raster.files=list.files(getwd(),  pattern=glob2rx(paste("*",this.species,"*_In.tif",sep="")), full.names=FALSE)
   
-  #get average price
-  
+  #' get average price
   species.price = species_data %>% 
-    filter(NewCode==this.species) %>% 
-    .$Price %>% 
-    as.numeric %>% .[1]
+  filter(NewCode==this.species) %>% 
+  .$Price %>% 
+  as.numeric %>% .[1]
   
   if(length(raster.files)==1)
   {
-    
     raster.species=stack(raster.files)
-  
-    #sum to get fishing intensity
-    
+    #' sum to get fishing intensity
     average.intensity = raster.species
     writeRaster(average.intensity, filename=paste(this.species,"cedo_Indx",sep=""), format="GTiff", overwrite=TRUE)  
-    #calculate economic index
+    #' calculate economic index
+    #' the economic index per species is equal to fishing intensity
+    #' only useful when combining species
     average.economic = average.intensity * species.price
     writeRaster(average.economic, filename=paste(this.species,"cedo_IE",sep=""), format="GTiff", overwrite=TRUE)  
   }
   
   if(length(raster.files)>1)
   {
-    
     raster.species=stack(raster.files)
-    
-   #sum to get fishing intensity
-    
+   #' sum to get fishing intensity
     average.intensity= mean(raster.species)
-    
     writeRaster(average.intensity, filename=paste(this.species,"cedo_Indx",sep=""), format="GTiff", overwrite=TRUE)  
-  #calculate economic index
+  #' calculate economic index
     average.economic = average.intensity * species.price
     writeRaster(average.economic, filename=paste(this.species,"cedo_IE",sep=""), format="GTiff", overwrite=TRUE)  
-    
+      }
     }
   
-  
-  }
-  
-# Now sum all rasters
+#' Now sum all rasters
   
 print("Summing all rasters")
-
-  setwd(pathToSaveShapes)
+setwd(pathToSaveShapes)
   
-   raster.files = list.files(getwd(),  pattern="cedo_Indx.tif$", full.names=FALSE)
-    raster.species.comm = stack(raster.files)
-    raster.species.comm[is.na(raster.species.comm)] <- 0
+raster.files = list.files(getwd(),  pattern="cedo_Indx.tif$", full.names=FALSE)
+raster.species.comm = stack(raster.files)
+raster.species.comm[is.na(raster.species.comm)] <- 0
     
-    index.intensity = sum(raster.species.comm)
+index.intensity = sum(raster.species.comm)
     
-    #write raster data
-    writeRaster(index.intensity, filename="cedo_intensity_index", format="GTiff", overwrite=TRUE)  
-    writeRaster(index.intensity, "cedo_intensity_index.asc", format="ascii",overwrite=TRUE)
+#' write raster data
+writeRaster(index.intensity, filename="cedo_intensity_index", format="GTiff", overwrite=TRUE)  
+writeRaster(index.intensity, "cedo_intensity_index.asc", format="ascii",overwrite=TRUE)
     
-    print("Saved intensity rasters") 
-    norm.data = raster("cedo_intensity_index.tif") %>% 
-      reclassify(cbind(0, NA)) 
+print("Saved intensity rasters") 
+norm.data = raster("cedo_intensity_index.tif") %>% 
+reclassify(cbind(0, NA)) 
    
-    #normalize raster 0-1 and save
-    norm.data = (index.intensity - cellStats(index.intensity,"min")) / (cellStats(index.intensity,"max")-cellStats(index.intensity,"min"))
+#' normalize raster 0-1 and save
+norm.data = (index.intensity - cellStats(index.intensity,"min")) / (cellStats(index.intensity,"max")-cellStats(index.intensity,"min"))
     
-    norm.data2 = norm.data %>% 
-      rasterToPoints %>% 
-      data.frame %>% 
-      setnames(c('Longitude', 'Latitude', 'Intensity'))
+norm.data2 = norm.data %>% 
+rasterToPoints %>% 
+data.frame %>% 
+setnames(c('Longitude', 'Latitude', 'Intensity'))
     
-    norm.data2[norm.data2 == 0] <- NA
-    norm.data2 =  norm.data2[complete.cases(norm.data2),]#eliminate rows with NA
+norm.data2[norm.data2 == 0] <- NA
+norm.data2 =  norm.data2[complete.cases(norm.data2),]#eliminate rows with NA
     
-    #create color palette
+#' create color palette
     
-    pal <- wes_palette(10, name = "FantasticFox", type = "continuous")
+pal <- wes_palette(10, name = "FantasticFox", type = "continuous")
     
-    #plot richness model and robustness
-    non.plot = ggplot(data=norm.data2, aes(y=Latitude, x=Longitude)) +
-      geom_raster(aes(fill=Intensity)) +
+#' plot richness model and robustness
+non.plot = ggplot(data=norm.data2, aes(y=Latitude, x=Longitude)) +
+geom_raster(aes(fill=Intensity)) +
       theme_bw() +
       coord_equal() + 
       scale_fill_gradientn(colours = pal)+
@@ -295,13 +255,13 @@ print("Summing all rasters")
             legend.text = element_text(size=10),
             legend.title = element_text(size=12))
     
-    setwd(mainpath)
-    #export as png
-    png("cedo_intensity.png")
-    non.plot
-    dev.off()
+setwd(mainpath)
+#' export as png
+png("cedo_intensity.png")
+non.plot
+dev.off()
     
-    # Sum all economic rasters
+    #' Sum all economic rasters
     
     setwd(pathToSaveShapes)
     
@@ -312,7 +272,7 @@ print("Summing all rasters")
     
     index.intensity = sum(raster.species.comm)
     
-    #write raster data
+    #' write raster data
     writeRaster(index.intensity, filename="cedo_economic_index", format="GTiff", overwrite=TRUE)  
     writeRaster(index.intensity, "cedo_economic_index.asc", format="ascii",overwrite=TRUE)
     
@@ -320,7 +280,7 @@ print("Summing all rasters")
         norm.data = raster("cedo_economic_index.tif") %>% 
       reclassify(cbind(0, NA)) 
     
-        #normalize raster 0-1 and save
+        #' normalize raster 0-1 and save
         norm.data = (index.intensity - cellStats(index.intensity,"min")) / (cellStats(index.intensity,"max")-cellStats(index.intensity,"min"))
         
     norm.data2 = norm.data %>% 
@@ -331,11 +291,11 @@ print("Summing all rasters")
     norm.data2[norm.data2 == 0] <- NA
     norm.data2 =  norm.data2[complete.cases(norm.data2),]#eliminate rows with NA
     
-    #create color palette
+    #' create color palette
     
     pal <- wes_palette(10, name = "FantasticFox", type = "continuous")
     
-    #plot richness model and robustness
+    #' plot richness model and robustness
     non.plot = ggplot(data=norm.data2, aes(y=Latitude, x=Longitude)) +
       geom_raster(aes(fill=Intensity)) +
       theme_bw() +
@@ -352,7 +312,7 @@ print("Summing all rasters")
             legend.title = element_text(size=12))
     
     setwd(mainpath)
-    #export as png
+    #' export as png
     png("cedo_economic.png")
     non.plot
     dev.off()
--- rfiles/organization_PANGAS_interviews_intensity.R
+++ rfiles/organization_PANGAS_interviews_intensity.R
@@ -1,50 +1,52 @@
-                                                                                                            # Hem Nalini Morzaria Luna hmorzarialuna@gmail.com
+#' November 2015
+#' organize shapefiles from PANGAS fishery participatory maps
+#' for spatial management project
+#' hmorzarialuna@gmail.com
+#' Last revised December 2015                                                                                                            # Hem Nalini Morzaria Luna hmorzarialuna@gmail.com
 # July 30, 2014
 # Organize datafiles and calculate fishing intensity
 
 x = c("gdata","ggplot2","reshape","dplyr","RColorBrewer","classInt",
-      "rgdal","rgeos","maptools","raster","data.table","wesanderson", "plyr")
-
+      "rgdal","rgeos","magrittr","maptools","raster","data.table","wesanderson", "plyr")
 lapply(x, require, character.only = TRUE)
 
+#' remove files and graphics
 rm(list=ls())
-
 graphics.off()
-setwd("E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Fishing_intensity")
-pathToSaveShapes = "E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Zonation/Scenarios_Nov2015/Original_data/"
-mainpath = "E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Zonation/"
+
+#' set working directories
+pathToSaveShapes = "E:/Archivos/1Archivos/Articulos/En preparacion/Spatial_management/Analisis/Zonation/Scenarios_Nov2015/Original_data/"
+mainpath = "E:/Archivos/1Archivos/Articulos/En preparacion/Spatial_management/Analisis/Zonation/"
 
 #define projection of data
 crs.geo <- CRS("+proj=lcc +lat_1=17.5 +lat_2=29.5 +lat_0=0 +lon_0=-102 +x_0=2000000 +y_0=0 +datum=NAD27 +units=m +no_defs")
 
+setwd("E:/Archivos/1Archivos/Articulos/En preparacion/Fisheries_intensity/Analysis/Fishing_intensity")
 #read data
 all.data <- readOGR(".", "all_interviews_v032710")
 
 setwd("E:/Archivos/1Archivos/Articulos/En preparacion/Fisheries_intensity")
 
-#Species names
+#' read table of species names
 species_data = fread("species_names.csv", header=TRUE)
 
-setwd("E:/Archivos/SIG/Proyectos/ArcGis/Datos ordenados/Shapes y layers/Archivos_articulos/Zonation/Scenarios_Nov2015/Frame")
+#' read shapefile of corridor and empty frame
+setwd("E:/Archivos/1Archivos/Articulos/En preparacion/Spatial_management/Analisis/Zonation/Scenarios_Nov2015/Frame")
 poly.r = readOGR(".", "corridor_wetland_polygon")
 north.r = raster("Frame_corridor_final.tif")
 #define projection of data
 proj.lcc = proj4string(north.r)
 crs.geo <- CRS(proj.lcc)
 
+#' make empty mask raster
+#' set background values to 0
 e.shape <- extent(north.r)
-#ras.rows = nrow(north.r)
-#ras.cols = ncol(north.r)
-
 mask.raster = raster(e.shape,resolution=c(1000,1000),crs=crs.geo)
-
-#set background values to 0
 mask.raster[is.na(mask.raster)] <- 0
 
+#' define communities in the corridor to subset
 community.list = c("PPE","SJO","PJA","STO","DDC","PLO")
-
 all.rg =subset(all.data, sitio_code %in% community.list) %>% drop.levels
-
 sp.levels = unique(all.rg$spp_code) %>% drop.levels
 
 for (eachspecies in 1:length(sp.levels)){
@@ -55,54 +57,51 @@ for (eachspecies in 1:length(sp.levels)){
     .$Price %>% 
     as.numeric
   
-  
   print(paste("Analyzing",this.species))
-  
     sp.corridor.com <- all.rg[all.rg$spp_code == this.species,]
-    
     setwd(pathToSaveShapes)
     
     unique.id <- unique(sp.corridor.com@data$Id) %>% as.character
     
     uid = 1
     
-    #get first polygon
-    
-    
+  #' get first polygon
       poly.data <- sp.corridor.com[sp.corridor.com$Id == unique.id[1], ] 
       n <- length(slot(poly.data, "polygons"))
       poly.data <- spChFIDs(poly.data, as.character(uid:(uid+n-1)))
       uid <- uid + n
-      
-      pol.area= gArea(poly.data)
+  #' calculates polygon area
+            pol.area= gArea(poly.data)
       poly.data$area = pol.area
-      
+  #' revalues codes assigned to polygons by fishers of area importance 
       revalue(poly.data$CODE, c("-" = 0, "A" = 1,"ABC"= 1, "B"  = 1, "C"= 1,   "Y"= 0.5,   "Y1"= 0.5,  "Y2"= 0.5,  "Y3"= 0.5,  "YA" = 0.5, "YB"= 0.5,  "YC" = 0.5, "YD"= 0.5))
-     
+  #' normalizes polygons by dividing polygon importance by area   
       poly.data$W= as.numeric(poly.data$CODE) / pol.area
-    
-
-    # mapunit polygon: combine remaining  polygons with first polygoan
+  #' revalues other polygons and combines with first polygoan
     
     if (length(unique.id)>1){
       
           for (i in 2:length(unique.id)) {
+      
       temp.data <- sp.corridor.com[sp.corridor.com$Id == unique.id[i], ] 
       n <- length(slot(temp.data, "polygons"))
       temp.data <- spChFIDs(temp.data, as.character(uid:(uid+n-1)))
       uid <- uid + n
-      
       pol.area= gArea(temp.data)
+      #' obtains polygon area
       temp.data$area = pol.area
-      
+      #' revalues codes and divides by area
       revalue(temp.data$CODE, c("-" = 0, "A" = 1,"ABC"= 1, "B"  = 1, "C"= 1,   "Y"= 0.5,   "Y1"= 0.5,  "Y2"= 0.5,  "Y3"= 0.5,  "YA" = 0.5, "YB"= 0.5,  "YC" = 0.5, "YD"= 0.5))
-      
       temp.data$W= as.numeric(temp.data$CODE) / pol.area
       poly.data <- spRbind(poly.data,temp.data)
-        
-          } 
+              } # end other polygons
       
-      poly.data$Indx = (poly.data$W - min(poly.data$W)) / (max(poly.data$W) - min(poly.data$W))
+      #' Normalize between min and 1
+      #' did not normalize between 0 and 1 because it effectively eliminates the 
+      #' minimum value, when being 0 it cannot be added to rasters from other
+      #' sources
+      #' This is the same thing Marcia did for her paper
+      poly.data$Indx = (poly.data$W) / (max(poly.data$W))
       
     } else if (length(unique.id)==1) {
       poly.data$Indx = 1
